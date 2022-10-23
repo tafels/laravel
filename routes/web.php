cdfa343e773e0532a54fe3664b999344823f59cf
Fedotov <tafels90@gmail.com>
2022-10-23 23:47:22 +0300

    version 0.01

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'MainController@index');
-Route::get('/mobile', 'MainController@category');
-Route::get('/mobile/iphone', 'MainController@product');
+Route::get('/{category}', 'MainController@category');
+Route::get('/mobile/{product}', 'MainController@product');
 
+Route::get('/', function () {
+    return view('index');
+});
